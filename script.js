8405b11554abee016c8d19f862b9d91df3ed0054
smsoeiro <51081616+smsoeiro@users.noreply.github.com>
2019-08-04 01:10:11 +0100

    add icon

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ function showWeather(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerText = Math.round(response.data.main.temp);
 
+  console.log(response.data);
+
   let place = document.querySelector("#place");
   place.innerText = response.data.name;
 
@@ -29,23 +31,30 @@ function showWeather(response) {
   description.innerText = response.data.weather[0].description;
 
   let humidity = document.querySelector("#humidity");
-  humidity.innerText = response.data.main.humidity;
+  humidity.innerText = Math.round(response.data.main.humidity);
 
   let windSpeed = document.querySelector("#windspeed");
-  windSpeed.innerText = response.data.wind.speed;
+  windSpeed.innerText = Math.round(response.data.wind.speed);
 
   let maxTemp = document.querySelector("#maxtemp");
-  maxTemp.innerText = response.data.main.temp_max;
+  maxTemp.innerText = Math.round(response.data.main.temp_max);
 
   let minTemp = document.querySelector("#mintemp");
   minTemp.innerText = Math.round(response.data.main.temp_min);
-  console.log(response.data);
 
   let date = document.querySelector("#date");
   date.innerText = formatDate(new Date(response.data.dt * 1000));
+
+  let icon = document.querySelector("#icon");
+
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/w/${response.data.weather[0].icon}.png `
+  );
 }
+
 let city = "Lisbon";
 let apiKey = "a56a00f6a5a3edb84cee6e71e1a24e77";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-axios.get(apiUrl).then(showTemperature);
+axios.get(apiUrl).then(showWeather);
